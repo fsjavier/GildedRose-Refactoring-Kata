6b132b404137e23dd0e70ca10bcc791edd98ebf5
fsjavier <javier.fersal@gmail.com>
2024-07-08 11:39:45 +0200

    Extract aged brie constant

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -7,6 +7,7 @@ class Item {
 }
 
 const SULFURAS = "Sulfuras, Hand of Ragnaros";
+const AGED_BRIE = "Aged Brie";
 class Shop {
   constructor(items = []) {
     this.items = items;
@@ -14,7 +15,7 @@ class Shop {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (
-        this.items[i].name != "Aged Brie" &&
+        this.items[i].name != AGED_BRIE &&
         this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
         if (this.items[i].quality > 0) {
@@ -45,7 +46,7 @@ class Shop {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
+        if (this.items[i].name != AGED_BRIE) {
           if (
             this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
           ) {
